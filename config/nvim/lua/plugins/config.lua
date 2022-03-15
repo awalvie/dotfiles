0fad42b68847d5e22e80425a08dd2c8cc1a8fe16
awalvie <agrawal.vishesh.178@gmail.com>
2022-03-15 08:35:48 +0530

    nvim: update lualine separator configs

--- config/nvim/lua/plugins/config.lua
+++ config/nvim/lua/plugins/config.lua
@@ -172,7 +172,7 @@ cmd([[
 -- Lualine config
 require('lualine').setup {
   options = {
-    section_separators = { left = ' ', right = ' '},
+    component_separators = { left = ' ', right = ' '},
     section_separators = { left = ' ', right = ' '},
     always_divide_middle = false,
     icons_enabled = false,

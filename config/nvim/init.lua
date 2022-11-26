3b64b5ee5046f7b50d6cadc24a574fa4d28e8f2d
awalvie <agrawal.vishesh.178@gmail.com>
2022-11-27 00:34:55 +0530

    nvim: use packer instead of paq

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,5 @@
 require('settings')
 require('mappings')
 require('plugins.list')
+require('plugins.config')
+
--- config/nvim/lua/plugins/list.lua
+++ config/nvim/lua/plugins/list.lua
@@ -1,49 +1,64 @@
----------------------------------------
--- Plugin list
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
 
-require 'paq' {
+local packer_bootstrap = ensure_packer()
 
-  -- Let Paq manage itself
-  "savq/paq-nvim";
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
   -- surround things with different things
-  'tpope/vim-surround';
-  -- File naivator
-  'scrooloose/nerdtree';
+  use 'tpope/vim-surround'
+  -- File navigator
+  use 'scrooloose/nerdtree'
   -- Main theme for vim
-  'morhetz/gruvbox';
+  use 'morhetz/gruvbox'
   -- Statusline theme for gruvbos
-  'nvim-lualine/lualine.nvim';
-  { 'kyazdani42/nvim-web-devicons', opt = true };
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires= { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
   -- Plugin for auto-completing closing brackets
-  'jiangmiao/auto-pairs';
+  use 'jiangmiao/auto-pairs'
   -- Plugin for quickly commenting out code
-  'preservim/nerdcommenter';
+  use 'preservim/nerdcommenter'
   -- Auto Code Formatter
-  'chiel92/vim-autoformat';
+  use 'chiel92/vim-autoformat'
   -- rip grep, yeah, I'm going into the corner
-  'jremmen/vim-ripgrep';
+  use 'jremmen/vim-ripgrep'
   -- git gutter, finally yes, I need to clean up this config sometime
-  'airblade/vim-gitgutter';
+  use 'airblade/vim-gitgutter'
   -- sit on that tree
-  'nvim-treesitter/nvim-treesitter';
-  'nvim-treesitter/playground';
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'nvim-treesitter/playground'
   -- telescope
-  'nvim-telescope/telescope.nvim';
-  'nvim-lua/plenary.nvim';
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-lua/plenary.nvim'
   -- lspconfig
-  'neovim/nvim-lspconfig';
+  use 'neovim/nvim-lspconfig'
   -- coq
-  { 'ms-jpq/coq_nvim', run = 'python3 -m coq deps' };
-  'ms-jpq/coq.artifacts';
+  use {
+    'ms-jpq/coq_nvim',
+    run = 'python3 -m coq deps'
+  }
+  use 'ms-jpq/coq.artifacts'
   -- yaml folding
-   'pedrohdz/vim-yaml-folds';
+  use 'pedrohdz/vim-yaml-folds'
 
   -- Language Plugins
-  'fatih/vim-go';
-  'rust-lang/rust.vim';
-}
+  use 'fatih/vim-go'
+  use 'rust-lang/rust.vim'
 
----------------------------------------
--- Source plugin specific settings
-
-require('plugins.config')
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)

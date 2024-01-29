2152660a527336e0e5e8d1ec1ceb264b75e4a0d5
awalvie <agrawal.vishesh.178@gmail.com>
2024-01-30 00:24:39 +0100

    nvim: migrate from packer to lazy for packaging

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,4 @@
-require('settings')
-require('mappings')
 require('plugins.list')
 require('plugins.config')
-
+require('settings')
+require('mappings')
--- config/nvim/lua/plugins/list.lua
+++ config/nvim/lua/plugins/list.lua
@@ -1,76 +1,66 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
-
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-
+require("lazy").setup({
   -- automatically add brackets
-  use 'm4xshen/autoclose.nvim'
+  'm4xshen/autoclose.nvim',
   -- surround things with different things
-  use 'tpope/vim-surround'
+  'tpope/vim-surround',
   -- File navigator
-  use 'scrooloose/nerdtree'
+  'scrooloose/nerdtree',
   -- Main theme for vim
-  use "nordtheme/vim"
+  "nordtheme/vim",
   -- Statusline theme for gruvbox
-  use {
+  {
     'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-  use {
-    'akinsho/bufferline.nvim',
-    tag = "*", requires = 'nvim-tree/nvim-web-devicons'
-  }
+    dependencies = { 'nvim-tree/nvim-web-devicons' }
+  },
+  {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
   -- Comform formatting
-  use 'stevearc/conform.nvim'
+  'stevearc/conform.nvim',
   -- Indent guide
-  use 'lukas-reineke/indent-blankline.nvim'
+  'lukas-reineke/indent-blankline.nvim',
   -- Plugin for quickly commenting out code
-  use 'preservim/nerdcommenter'
+  'preservim/nerdcommenter',
   -- rip grep, yeah, I'm going into the corner
-  use 'jremmen/vim-ripgrep'
+  'jremmen/vim-ripgrep',
   -- git gutter, finally yes, I need to clean up this config sometime
-  use 'airblade/vim-gitgutter'
+  'airblade/vim-gitgutter',
   -- sit on that tree
-  use 'nvim-treesitter/nvim-treesitter'
+  'nvim-treesitter/nvim-treesitter',
   -- telescope
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  }
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+  },
   -- lspconfig
-  use 'williamboman/mason.nvim'
-  use 'neovim/nvim-lspconfig'
+  'williamboman/mason.nvim',
+  'neovim/nvim-lspconfig',
   -- yaml folding
-  use 'pedrohdz/vim-yaml-folds'
+  'pedrohdz/vim-yaml-folds',
   -- completion
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
-  use 'L3MON4D3/LuaSnip'
-  use 'saadparwaiz1/cmp_luasnip'
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  'hrsh7th/nvim-cmp',
+  'L3MON4D3/LuaSnip',
+  'saadparwaiz1/cmp_luasnip',
 
   -- Language Plugins
-  use 'godlygeek/tabular'
-  use 'preservim/vim-markdown'
+  'godlygeek/tabular',
+  'preservim/vim-markdown',
 
   -- Copilot
-  use 'github/copilot.vim'
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+  'github/copilot.vim',
+})

eb6fa76dc7d08d92ecbbb34aa5d3b0765ebb1b05
awalvie <agrawal.vishesh.178@gmail.com>
2024-01-30 15:00:36 +0100

    nvim: remove Text suggestions from nvim_lsp for cmp

--- config/nvim/lua/plugins/config.lua
+++ config/nvim/lua/plugins/config.lua
@@ -178,7 +178,12 @@ cmp.setup({
 
   }),
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
+    {
+      name = 'nvim_lsp',
+      entry_filter = function(entry, ctx)
+        return require('cmp.types').lsp.CompletionItemKind[entry:get_kind()] ~= 'Text'
+      end
+    },
     { name = 'luasnip' },
     { name = 'path' },
     { name = 'buffer' },

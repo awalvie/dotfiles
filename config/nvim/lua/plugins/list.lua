048a5e6c423e765a3ce97580eace50d687e7478d
awalvie <agrawal.vishesh.178@gmail.com>
2024-04-15 09:10:03 +0200

    nvim: add neodev.nvim

--- config/nvim/lua/plugins/list.lua
+++ config/nvim/lua/plugins/list.lua
@@ -130,5 +130,20 @@ require("lazy").setup({
   "backdround/global-note.nvim",
 
   -- maker of plugins, breaker of vim
-  { "folke/neodev.nvim",       opts = {} }
+  { "folke/neodev.nvim",       opts = {} },
+
+  -- figure out what I'm doing
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
 })

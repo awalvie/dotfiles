d816fc09db4da5b4aacf692341f9e34ef423867f
awalvie <awalvie@meidua.localdomain>
2025-10-09 16:31:37 +0200

    nvim: update telescope

--- config/nvim/lua/plug.lua
+++ config/nvim/lua/plug.lua
@@ -77,12 +77,12 @@ require("lazy").setup({
 	},
 	{
 		'nvim-telescope/telescope.nvim',
-		tag = '0.1.5',
+		tag = '0.1.8',
 		dependencies = {
 			'nvim-lua/plenary.nvim',
 			{
 				'nvim-telescope/telescope-fzf-native.nvim',
-				build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
+				build = 'make',
 			},
 			{
 				"AckslD/nvim-neoclip.lua",

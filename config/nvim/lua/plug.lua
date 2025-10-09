3e2958b5c78a68a0631c6978cc395c159b6af065
awalvie <awalvie@meidua.localdomain>
2025-10-09 15:58:37 +0200

    nvim: minor fix

--- config/nvim/lua/plug.lua
+++ config/nvim/lua/plug.lua
@@ -82,7 +82,7 @@ require("lazy").setup({
 			'nvim-lua/plenary.nvim',
 			{
 				'nvim-telescope/telescope-fzf-native.nvim',
-				build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
+				build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
 			},
 			{
 				"AckslD/nvim-neoclip.lua",

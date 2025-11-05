8f59647e945786dddd120bad94da3d391a2512e8
Vishesh agrawal <agrawal.vishesh.178@gmail.com>
2025-11-05 15:44:32 +0100

    nvim: enable telescope stuff

--- config/nvim/lua/plugins/config/telescope.lua
+++ config/nvim/lua/plugins/config/telescope.lua
@@ -13,7 +13,7 @@ require('telescope').setup {
 			vertical = { mirror = false },
 		},
 		winblend = 0,
-		border = false,
+		border = true,
 		borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
 		color_devicons = false,
 	},
@@ -22,12 +22,12 @@ require('telescope').setup {
 	},
 	extensions = {
 		fzf = {
-		  fuzzy = true,                    -- false will only do exact matching
-		  override_generic_sorter = true,  -- override the generic sorter
-		  override_file_sorter = true,     -- override the file sorter
-		  case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-	   }
-	 }
+			fuzzy = true,           -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+		}
+	}
 }
 
 require('telescope').load_extension('fzf')

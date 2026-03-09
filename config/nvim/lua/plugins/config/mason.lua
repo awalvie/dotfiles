f4267b8c10b407c11d45df2a7c29d0f8ca7ec3fb
hadrian <hadrian@goddodin.tail13fe4.ts.net>
2026-03-09 13:15:03 +0100

    nvim: update mason on start

--- config/nvim/lua/plugins/config/mason.lua
+++ config/nvim/lua/plugins/config/mason.lua
@@ -1,2 +1,18 @@
 -- mason.lua
 require("mason").setup()
+
+-- Auto-update all installed Mason packages in the background on startup
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local registry = require("mason-registry")
+    registry.refresh(function()
+      for _, pkg in ipairs(registry.get_installed_packages()) do
+        local installed = pkg:get_installed_version()
+        local latest = pkg:get_latest_version()
+        if installed ~= latest then
+          pkg:install()
+        end
+      end
+    end)
+  end,
+})

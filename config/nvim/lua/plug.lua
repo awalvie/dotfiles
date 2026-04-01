830a3b97fb8550cfdb63d331a36b4b60541e40f8
Vishesh agrawal <agrawal.vishesh.178@gmail.com>
2026-04-02 00:15:23 +0200

    nvim: remove lazygit
    Signed-off-by: Vishesh agrawal <agrawal.vishesh.178@gmail.com>

--- config/nvim/lua/plug.lua
+++ config/nvim/lua/plug.lua
@@ -50,22 +50,6 @@ require("lazy").setup({
 		"lewis6991/gitsigns.nvim",
 	},
 	{
-		"kdheepak/lazygit.nvim",
-		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
-		},
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		keys = {
-			{ "<leader>gl", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-		},
-	},
-	{
 		'nvim-treesitter/nvim-treesitter',
 		dependencies = { 'nvim-treesitter/nvim-treesitter-context' },
 	},

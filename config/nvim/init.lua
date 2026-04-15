ee7dd471a14c729edb864e70d44821cc0ad3cb52
Vishesh agrawal <agrawal.vishesh.178@gmail.com>
2026-04-15 10:53:24 +0200

    nvim: enable ui2

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,6 +3,26 @@
 -- Set leader key early
 vim.g.mapleader = ","
 
+-- Enable the experimental Neovim 0.12 message/cmdline UI when available.
+do
+	local ok, ui2 = pcall(require, 'vim._core.ui2')
+	if ok then
+		ui2.enable({
+			enable = true,
+			msg = {
+				targets = 'cmd',
+				msg = {
+					timeout = 4000,
+					height = 0.5,
+				},
+				pager = {
+					height = 1.0,
+				},
+			},
+		})
+	end
+end
+
 -- Load config + plugins
 require('options')
 require('keymaps')
